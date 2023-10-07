358d1bfbc3838c9c1a16ed37b0a94e503bb4aefb
trang <trangbg2000@gmail.com>
2023-10-07 19:43:55 +0700

    init project

---
+++ app/src/main/java/com/example/quizpractice/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizpractice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+    }
+
+}
