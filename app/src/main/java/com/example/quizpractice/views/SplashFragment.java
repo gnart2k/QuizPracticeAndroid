6fff76a5dc5c357007c4b784a767d605ddf57570
trang <trangbg2000@gmail.com>
2023-10-15 21:11:22 +0700

    done splash layout

---
+++ app/src/main/java/com/example/quizpractice/views/SplashFragment.java
@@ -0,0 +1,29 @@
+package com.example.quizpractice.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashFragment extends Fragment {
+
+
+    //private AuthViewModel viewModel;
+    private NavController navController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
