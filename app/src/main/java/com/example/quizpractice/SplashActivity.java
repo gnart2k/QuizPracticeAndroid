85a31a2439a335f3776fac0847a286ed8d5f1f57
trang <trangbg2000@gmail.com>
2023-10-15 19:18:45 +0700

    merged

--- app/src/main/java/com/example/quizpractice/SplashActivity.java
+++ app/src/main/java/com/example/quizpractice/SplashActivity.java
@@ -1,14 +1,10 @@
 package com.example.quizpractice;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spash);
-    }
 
 }
