8244c2e7eeb1f51534b1326b5384279c97609242
Huy Do <quanghuytrn204@gmail.com>
2023-10-21 21:53:00 +0700

    add quiz list

--- app/src/main/java/com/example/quizpractice/views/SignInFragment.java
+++ app/src/main/java/com/example/quizpractice/views/SignInFragment.java
@@ -61,6 +61,6 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in2, container, false);
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 }
