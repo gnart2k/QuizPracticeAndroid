7be13ab1357bedc668ef7d3e902441f699fc1930
lap <lapdche150837@fpt.edu.vn>
2023-10-22 00:57:59 +0700

    Fetching Quiz Questions

---
+++ app/src/main/java/com/example/quizpractice/viewmodel/QuestionViewModel.java
@@ -0,0 +1,41 @@
+package com.example.quizpractice.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.quizpractice.Model.QuestionModel;
+import com.example.quizpractice.repository.QuestionRepository;
+
+import java.util.List;
+
+public class QuestionViewModel extends ViewModel implements QuestionRepository.OnQuestionLoad {
+
+
+    private MutableLiveData<List<QuestionModel>> questionMutableLiveData;
+    private QuestionRepository repository;
+
+    public MutableLiveData<List<QuestionModel>> getQuestionMutableLiveData() {
+        return questionMutableLiveData;
+    }
+
+    public QuestionViewModel(){
+        questionMutableLiveData = new MutableLiveData<>();
+        repository = new QuestionRepository(this);
+    }
+
+    @Override
+    public void onLoad(List<QuestionModel> questionModels) {
+        questionMutableLiveData.setValue(questionModels);
+    }
+
+    public  void setQuizId(String quizId){
+        repository.setQuizID(quizId);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        Log.d("QuizError", "onError: " + e.getMessage());
+    }
+}
